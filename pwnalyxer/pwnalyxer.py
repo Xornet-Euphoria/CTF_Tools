4d06baf58ba73d5a5a96a44470817fe60db49ca2
Xornet-Euphoria <einnotwendigesubel@gmail.com>
2019-10-26 17:21:10 +0900

    引数を適当に追加

--- pwnalyxer/pwnalyxer.py
+++ pwnalyxer/pwnalyxer.py
@@ -12,45 +12,72 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="analyzing binary tool for pwn")
     parser.add_argument("elf", help="binary path")
     parser.add_argument("-l", "--libc", help="libc path")
+    parser.add_argument("-s", "--simple", help="only show infomation of section and symbols (not include gadgets and ignore libc)", action="store_true")
+    parser.add_argument("--disas", help="disassemble specified function by name (in current version, function only)")
+    
+    # under construction
+    # 目標はradare2の劣化版みたいな解析機能
+    parser.add_argument("--detail", help="analyze binary for detail information", action="store_true")
 
     args = parser.parse_args()
-    # todo: 引数(実行ファイル、libc, 各項目分離)
-    #     : 結果のテキストファイルへの保存
+    # todo: 引数(実行ファイル)
+    #     : 結果のテキストファイルへの保存 -> リダイレクトで流せばよくない?
     elf = ELF(args.elf)  # _write4
     libc = ELF(args.libc) if args.libc else None
 
-    print("")
-    # dump sections
-    print("[+]: all sections")
     sect = Section(elf)
-    sect.dump_all_sections()
-
-    print("")
-    # all functions
-    # todo: 関数の中身を調べる方法(ローカル変数の数やできるなら初期値も調べたい)
-    print("[+]: all functions")
     functions = Functions(elf)
-    functions.dump_all_functions()
-    
-
-    print("")
-    # plt
-    print("[+]: plt")
     plt = elf.plt
-    table_header = "{0:30} : {1:15}"
-    print(table_header.format("symbol name", "address"))
-    print("-" * 50)
-    for p in plt.items():
-        print(table_header.format(p[0], hex(p[1])))
-
-    print("")
-    # got
-    print("[+]: got")
     got = elf.got
-    print(table_header.format("symbol name", "address"))
-    print("-" * 50)
-    for g in got.items():
-        print(table_header.format(g[0], hex(g[1])))
+
+    # dump simple
+    if not args.disas:
+        print("")
+        # dump sections
+        print("[+]: all sections")
+        sect.dump_all_sections()
+
+        print("")
+        # all functions
+        # todo: 関数の中身を調べる方法(ローカル変数の数やできるなら初期値も調   べたい)
+        print("[+]: all functions")
+        functions.dump_all_functions()
+
+        # plt
+        print("")
+        print("[+]: plt")
+        table_header = "{0:30} : {1:15}"
+        print(table_header.format("symbol name", "address"))
+        print("-" * 50)
+        for p in plt.items():
+            print(table_header.format(p[0], hex(p[1])))
+
+        print("")
+        # got
+        print("[+]: got")
+        print(table_header.format("symbol name", "address"))
+        print("-" * 50)
+        for g in got.items():
+            print(table_header.format(g[0], hex(g[1])))
+    
+    elif args.disas:
+        target = args.disas
+        print("")
+        target_f = functions.search_function_by_name(target)
+        if target_f:
+            print("[+]: disassenble function `{}`".format(target))
+            target_f.dump_disas()
+        else:
+            print("[+]: the function `{}` is not found. Please check function name.".format(target))
+            print("[+]: all functions are here.")
+            functions.dump_all_functions()
+        exit(0)
+
+    if args.simple:
+        if libc:
+            print("")
+            print("[notice]: --libc option is set, but ignored")
+        exit(0)
 
     print("")
     # rp++
