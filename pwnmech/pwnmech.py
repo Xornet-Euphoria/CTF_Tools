88d8fa389c0fc9516adfe06ddc60e35f786122bc
Xornet-Euphoria <einnotwendigesubel@gmail.com>
2020-02-06 02:30:18 +0900

    shellcode without null byte

--- pwnmech/pwnmech.py
+++ pwnmech/pwnmech.py
@@ -1,3 +1,7 @@
+from binascii import hexlify
+from pwn import p32, p64
+
+
 def check_payload(payload, input_function=None, max_length=None, stop=False, log=False):
     error_count = 0
     # check black list char
@@ -7,7 +11,7 @@ def check_payload(payload, input_function=None, max_length=None, stop=False, log
     elif input_function == "scanf":
         blacklist = list(range(0x09, 0x0d + 1))
         blacklist.append(0x20)
-    elif input_function == "read": 
+    elif input_function == "read":
         blacklist = []
     else:
         raise ValueError
@@ -18,12 +22,77 @@ def check_payload(payload, input_function=None, max_length=None, stop=False, log
             error_count += 1
 
             if log:
-                print("[+] invalid char of %s: %x (`%c`)" % (input_function, int.from_bytes(c, "little"), int.from_bytes(c, "little")))
+                print("[+] invalid char of %s: %x (`%c`)" % (input_function,
+                                                             int.from_bytes(c, "little"), int.from_bytes(c, "little")))
 
     if max_length is not None:
         if len(payload) > max_length:
             if log:
-                print("[+] length must be less than or equal to %d" % max_length)
+                print("[+] length must be less than or equal to %d" %
+                      max_length)
             error_count += 1
 
     return error_count
+
+
+"""
+    ================= tools for shellcode =================
+"""
+
+def str_to_long(s):
+    return int(hexlify(s.encode()), 16)
+
+
+def split_string(s, x64=False):
+    word = 8 if x64 else 4
+    s += "\x00"
+
+    while len(s) % word != 0:
+        s += "\x00"
+
+    pushs = []
+    index = len(s)
+    while index >= word:
+        pushs.append(s[index - word:index][::-1])
+        index -= word
+
+    return pushs
+
+
+def push_string(s, x64=False):
+    shellcode = ""
+
+    pushs = split_string(s, x64)
+
+    for operand in pushs:
+        mnemonic = push_without_null(str_to_long(operand))
+        shellcode += mnemonic
+
+    return shellcode
+
+
+def push_without_null(num, reg="rax", x64=False):
+    f = p64 if x64 else p32
+    packed_num = f(num)
+
+    if b"\x00" not in packed_num:
+        return "push 0x%x\n" % (num)
+
+    # mov <reg>, <non zero num1>; xor <reg>, <non zero num2>; push <reg>
+    bytes1 = b""
+    bytes2 = b""
+
+    for b in packed_num:
+        for b1 in range(1, 256):
+            b2 = b ^ b1
+            if b2 != 0:
+                bytes1 += b1.to_bytes(1, "big")
+                bytes2 += b2.to_bytes(1, "big")
+                break
+                
+                
+    shellcode = "mov %s, 0x%x\n" % (reg, int(hexlify(bytes1), 16))
+    shellcode += "xor %s, 0x%x\n" % (reg, int(hexlify(bytes2), 16))
+    shellcode += "push %s\n" % reg
+
+    return shellcode
